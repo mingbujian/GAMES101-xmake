81ab59b783e39e8e4bc3b3118a7e01fa5ba7b411
mingbujian <mingbujian@foxmail.com>
2024-05-04 19:58:18 +0800

    作业1 提高项

--- src/Assignment1/main.cpp
+++ src/Assignment1/main.cpp
@@ -42,28 +42,69 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
                                       float zNear, float zFar)
 {
     // Students will implement this function
-
+    zNear = -zNear;
+    zFar = -zFar;
     Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
 
     // TODO: Implement this function
     // Create the projection matrix for the given parameters.
     // Then return it.
-    Eigen::Matrix4f perspective;
+    Eigen::Matrix4f perspective;//透视投影
     perspective << zNear, 0, 0, 0
                 , 0, zNear, 0, 0
-                , 0, 0, zNear + zFar, 0
+                , 0, 0, zNear + zFar, -1*zNear*zFar
                 , 0, 0, 1, 0;
 
     float height = 2*std::tan(eye_fov/2)*std::fabs(zNear);
     float width = aspect_ratio * height;
 
-    Eigen::Matrix4f orthographic;
+    Eigen::Matrix4f orthographic;//正交投影
     orthographic << 2/width, 0, 0, 0
                     , 0, 2/height, 0, 0
                     , 0, 0, 2/(zFar-zNear), -(zFar+zNear)/2
                     , 0, 0, 0, 1;
+                    
+    Eigen::Matrix4f transform;//正交投影需要先移动到原点
+    transform << 1, 0, 0, 0
+                , 0, 1, 0, 0
+                , 0, 0, 1, -(zNear+zFar)/2
+                , 0, 0, 0, 1;
 
-    projection = orthographic * perspective * projection;
+    projection = orthographic * transform * perspective * projection;
+    return projection;
+}
+
+Eigen::Matrix4f get_rotation(Vector3f axis, float angle)
+{
+    float radian = angle/180 * std::acos(-1);
+    float nx = axis[0];
+    float ny = axis[1];
+    float nz = axis[2];
+
+    // 1,0,0绕axis旋转得到的向量
+    Vector3f p1 = {std::cos(radian) + (1-std::cos(radian))*nx*nx, (1-std::cos(radian))*nx*ny-std::sin(radian)*nz, (1-std::cos(radian))*nx*ny+std::sin(radian)*ny};
+    // 0,1,0绕axis旋转得到的向量
+    Vector3f p2 = {(1-std::cos(radian))*ny*nx + std::sin(radian)*nz, std::cos(radian)+(1-std::cos(radian))*ny*ny,(1-std::cos(radian))*ny*nz - std::sin(radian)*nx};
+    // 0,0,1绕axis旋转得到的向量
+    Vector3d p3 = {(1-std::cos(radian))*nx*nz-std::sin(radian)*ny, (1-std::cos(radian))*ny*nz+std::sin(radian)*nx, std::cos(radian)+(1-std::cos(radian))*nz*nz};
+
+    // 先求(1,0,0)绕axis旋转的Q矩阵，Q的转置/逆，就是我们需要的绕axis的旋转矩阵了
+    Eigen::Matrix4f Q;
+    Q << p1[0], p2[0], p3[0], 0
+        , p1[1], p2[1], p3[1], 0
+        , p1[2], p2[2], p3[2], 0
+        , 0, 0, 0, 1;
+
+    Eigen::Matrix4f M;
+    M << 1, 0, 0, 0
+        , 0, 1, 0, 0
+        , 0, 0, 1, 2
+        , 0, 0, 0, 1;
+
+    Eigen::Matrix4f Qt = Q.inverse();
+
+    Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
+    projection = M.inverse() * Qt * M * projection;
     return projection;
 }
 
@@ -86,6 +127,7 @@ int main(int argc, const char** argv)
     rst::rasterizer r(700, 700);
 
     Eigen::Vector3f eye_pos = {0, 0, 5};
+    Eigen::Vector3f axis = {0,1.414/2, -1.414/2};
 
     std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
 
@@ -116,7 +158,9 @@ int main(int argc, const char** argv)
     while (key != 27) {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
-        r.set_model(get_model_matrix(angle));
+        //r.set_model(get_model_matrix(angle));
+        r.set_model(get_rotation(axis, angle));
+
         r.set_view(get_view_matrix(eye_pos));
         r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
@@ -127,13 +171,24 @@ int main(int argc, const char** argv)
         cv::imshow("image", image);
         key = cv::waitKey(10);
 
-        std::cout << "frame count: " << frame_count++ << '\n';
+        std::cout << "frame count: " << axis << '\n';
 
         if (key == 'a') {
             angle += 10;
         }
         else if (key == 'd') {
             angle -= 10;
+        }
+
+        if (key == 'x') {
+            axis[0] += 1;
+        }
+        else if (key == 'y') {
+            axis[1] += 1;
+        }
+        else if(key == 'z')
+        {
+            axis[2] += 1;
         }
     }
 
